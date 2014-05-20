833318c961d22cdf554dc0fb836057bf465f8f08
Chris Herbst <cherbst@mdsol.com>
2014-05-20 12:31:02 -0400

    Drop some unused requires.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,8 @@ rails_root = File.dirname(__FILE__) + '/../rails_root'
 migration_dir= "#{rails_root}/db/migrate"
 
 require "#{rails_root}/config/environment.rb"
-
-require 'rails_generator'
-require 'rails_generator/scripts/generate'
-require 'rails_generator/scripts/destroy'
 require 'fileutils'
 require 'ruby-debug'
-require 'spec'
 
 module MedidataButtonsHelpers
 end
