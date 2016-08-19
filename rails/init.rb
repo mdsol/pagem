b090955f0431cfd439794d8f122d5ecc94fbd33c
Bob White <bwhite@mdsol.com>
2016-08-19 08:17:02 -0400

    gemify assets

--- rails/init.rb
+++ rails/init.rb
@@ -1,16 +1,16 @@
-require "pagem"
-
-['/public/stylesheets/pagem.css'].each do |file|
-    source = File.join(directory,file)
-    dest = RAILS_ROOT + file
-    FileUtils.cp(source, dest)
-end
-
-
-# move images to public/images/medidata_buttons
-['/public/images/pagem'].each do |dir|
-  source = File.join(directory,dir)
-  dest = RAILS_ROOT + dir
-  FileUtils.mkdir_p(dest)
-  FileUtils.cp(Dir.glob(source+'/*.*'), dest)
-end
+# require "pagem"
+#
+# ['/public/stylesheets/pagem.css'].each do |file|
+#     source = File.join(directory,file)
+#     dest = RAILS_ROOT + file
+#     FileUtils.cp(source, dest)
+# end
+#
+#
+# # move images to public/images/medidata_buttons
+# ['/public/images/pagem'].each do |dir|
+#   source = File.join(directory,dir)
+#   dest = RAILS_ROOT + dir
+#   FileUtils.mkdir_p(dest)
+#   FileUtils.cp(Dir.glob(source+'/*.*'), dest)
+# end
